f186ce97ad40ce2f4ea823802b68fa1995d84614
wtjerry <wtjerry123@gmail.com>
2015-12-28 14:32:29 +0100

    made player number variable

--- srv/srv.py
+++ srv/srv.py
@@ -12,27 +12,39 @@ import os
 # ************** functions
 
 def createPlayerHomeDir(playerDir):
+	print("trying to create home dir: ", playerDir) 
 	if not os.path.exists(playerDir):
 		os.mkdir(playerDir)
+		print("created home dir: ", playerDir)
+	else:
+		print("home dir already available: ", playerDir)
 
 def createPipe(playerDir):
 	queuePath = os.path.join(playerDir, "command-queue")
+	print("trying to create queue: ", queuePath)
 	if not os.path.exists(queuePath):
 		os.mkfifo(queuePath)
+		print("created queue: ", queuePath)
+	else:
+		print("queue already available: ", queuePath)
+
+
 
 
 # ************** start
 
-print("hello world, no just jocking getting to work now...")
+print("hello world, no just jocking going to work now...")
 
-playerDir1 = sys.argv[1]
-playerDir2 = sys.argv[2]
 
-createPlayerHomeDir(playerDir1)
-createPipe(playerDir1)
+numberOfPlayers = len(sys.argv)
+for i in range(1, numberOfPlayers):
 
-createPlayerHomeDir(playerDir2)
-createPipe(playerDir2)
+	playerDir = sys.argv[i]
+	print("player dir: ", playerDir)
+	
+	createPlayerHomeDir(playerDir)
+	createPipe(playerDir)
+
 
 print("started")	
 
