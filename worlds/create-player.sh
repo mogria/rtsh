a5c47c8915dec3c9c6941842ae0fd64b8a718b0f
Mogria <m0gr14@gmail.com>
2015-12-28 17:12:55 +0100

    move create-player.sh to world container so both the client and server container can use the same script

--- worlds/create-player.sh
+++ worlds/create-player.sh
@@ -4,12 +4,21 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+NEWUID=9001
+
 # TODO: all users are created without a password. We need some way
 # to register users. so this script can get these passwords.
 
 
+while ! cut -d: -f1 | grep -E "^$NEWUID\$"; do
+    NEWUID="$((NEWUID + 1))"
+done
+
 useradd --password "" \
         --create-home  \
+        --uid "$NEWUID"
         --home-dir "/home/$1" \
+        --groups rtshplayers \
         "$1"
+
 
