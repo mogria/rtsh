81a45e8ec9656615cce2e1e5c984ce1ceaee0425
Mogria <m0gr14@gmail.com>
2015-12-28 15:26:32 +0100

    add create player script

---
+++ srv/create-player.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Usage: $0 playername"
+    exit 1
+fi
+
+# TODO: all users are created without a password. We need some way
+# to register users. so this script can get these passwords.
+
+
+useradd --password "" \
+        --create-home  \
+        --home-dir "/home/$1" \
+        "$1"
+
