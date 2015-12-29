560a5e71f830479e37b0eeec166241830e79c378
Mogria <m0gr14@gmail.com>
2015-12-29 16:23:07 +0100

    fix start script for wetty client, to create not only one player

--- wetty-cli/start-script.sh
+++ wetty-cli/start-script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-echo args "$@"
 for player in "$@"; do
-    echo "creating player .... [$1]"
-    /home/create-player.sh -n "$1"
+    /home/create-player.sh -n "$player"
 done
 
 exec node app.js -p 3000
