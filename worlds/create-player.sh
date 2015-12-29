c4f92321d0eb4aa3251235af7d9105c579f7db98
Mogria <m0gr14@gmail.com>
2015-12-29 15:27:53 +0100

    try to fix create player script

--- worlds/create-player.sh
+++ worlds/create-player.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+
+
 if [ -z "$1" ]; then
-    echo "Usage: $0 playername"
+    echo "Usage: $0 [-n] playername"
+    echo "pass the -n option if you don't want to create the users home directory"
     exit 1
 fi
+
+OPTIND=1 # reset getopts
+
+CREATE_HOME="--create-home"
+while getopts "h?" opt; do
+    case "$opt" in
+        n) CREATE_HOME="-m"
+           ;;
+    esac
+done
+
+shift "$((OPTIND - 1))" # remove all non-positional arguments
 
 NEWUID=9001
 
@@ -10,12 +25,12 @@ NEWUID=9001
 # to register users. so this script can get these passwords.
 
 
-while ! cut -d: -f3 "/etc/passwd" | grep -E "^$NEWUID\$"; do
+while ! cut -d: -f3 "/etc/passwd" | grep -E "^$NEWUID$"; do
     NEWUID="$((NEWUID + 1))"
 done
 
 useradd --password "" \
-        --create-home  \
+        $CREATE_HOME
         --uid "$NEWUID" \
         --home-dir "/home/$1" \
         --groups rtshplayers \
