07e97704a495c205673cf11c61298f52283e6a08
Mogria <m0gr14@gmail.com>
2016-01-06 16:49:35 +0100

    add genfunc.sh for some basic functions to generate a map
    Also added the world.json file which contains info about the world
    generated.

--- worlds/gen.sh
+++ worlds/gen.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then
-    echo "usage: $0 size_x size_y";
+LOC="$( cd "$(dirname "$0")" && pwd)"
+. "$LOC/genfunc.sh"
+
+if [ "$#" -lt 3 ]; then
+    echo "usage: $0 worldname size_x size_y [terrain_generator]";
     exit 0
 fi
 
-SIZEX="$1"
-SIZEY="$2"
+WORLDNAME="$1"
+SIZEX="$2"
+SIZEY="$3"
+TERRAIN_GENERATOR="${4:-random}"
+SEED="$RANDOM"
 
-TERRAIN_TYPES=("grass" "desert" "woods" "plain")
+echo "Generating world (${SIZEX}x$SIZEY) with '$TERRAIN_GENERATOR' generator (seed: $SEED) ..."
 
-echo "generating World (${SIZEX}x$SIZEY)..."
+new_world "$WORLDNAME" "$SIZEX" "$SIZEY" "$TERRAIN_GENERATOR" "$SEED" > world.json
 
-for ((y = 0; x < SIZEY; y++)); do
+for ((y = 0; y < SIZEY; y++)); do
     for ((x = 0; x < SIZEX; x++)); do
         mkdir -p "$x/$y"
-        terrain="${TERRAIN_TYPES[ $RANDOM % ${#TERRAIN_TYPES[@]} ] }"
-        cat > "$x/$y/terrain.json" <<terrainjson
-{
-    "class": "terrain",
-    "type": "$terrain"
-}
-terrainjson
+        terrain="$(terrain_generator_${TERRAIN_GENERATOR} "$x" "$y")"
+        new_terrain "$terrain" > "$x/$y/terrain.json"
+        mkdir "$x/$y/units"
     done
 done
 
---
+++ worlds/genfunc.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+containsElement() {
+    local e
+    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+    return 1
+}
+
+set_attr_str() {
+    jq --arg attr "$2" ".$1 = \$attr"
+}
+
+set_attr() {
+    jq --argjson attr "$2" ".$1 = \$attr"
+}
+
+new() {
+    if [ "$#" -ne 1 ]; then
+        return 1
+    fi
+
+    CLASSES=( "terrain"  "world" "unit" "building" )
+    if containsElement "$1" "${CLASSES[@]}"; then
+        echo '{}' | set_attr_str class "$1"
+        return 0
+    fi
+    return 1
+}
+
+new_terrain() {
+    if containsElement "$1" "${TERRAIN_TYPES[@]}"; then
+        new "terrain" | set_attr_str type "$1"
+        return 0
+    fi
+    return 1
+}
+
+new_world() {
+    if [ "$#" -lt 3 ]; then
+        return 1
+    fi
+
+    output="$(new "world" | set_attr_str name "$1" | set_attr size_x "$2" | set_attr size_y "$3")"
+    
+    if [ "$#" -ge 4 ]; then
+        output="$(echo "$output" | set_attr_str terrain_generator "$4")"
+    fi
+    if [ "$#" -ge 5 ]; then
+        output="$(echo "$output" | set_attr seed "$5")"
+    fi
+    echo "$output"
+}
+
+TERRAIN_TYPES=("grass" "desert" "woods" "plain")
+
+terrain_generator_random() {
+    terrain="${TERRAIN_TYPES[ $RANDOM % ${#TERRAIN_TYPES[@]} ] }"
+    echo -n "$terrain"
+}
