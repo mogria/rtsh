b4c64c7bcc04f8857513e21d771de53c6d834b2b
Mogria <m0gr14@gmail.com>
2016-01-18 09:31:51 +0100

    add a setup.py file for the game server

---
+++ srv/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'rtsh.srv',
+    version = '0.1',
+    description = 'Python Server for the Real Time Strategy Shell Game',
+    author = 'wtjerry, mogria',
+    author_email = 'wtjerry123@gmail.com, m0gr14@gmail.com',
+    keywords = 'real-time strategy game server',
+    url = 'https://github.com/mogria/rtsh',
+
+    packages = find_packages(),
+    scripts = ['srv.py']
+)
+
