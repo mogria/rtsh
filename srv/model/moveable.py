402fd55bbdee0f37ec9db65d36b77b5099536396
Mogria <m0gr14@gmail.com>
2016-01-20 18:06:30 +0100

    add test for moveable.py, correct and simplify path finding method

--- srv/model/moveable.py
+++ srv/model/moveable.py
@@ -13,36 +13,63 @@ class Moveable(Positionable):
     def move_speed(self):
         return self._move_speed
 
+    @property
+    def move_cycle(self):
+        return self._move_cycle
+
+    @property
+    def move_target(self):
+        return self._move_target
+
+    @move_target.setter
+    def move_target(self, move_target):
+        # TODO: check for valid coordinates
+        self._move_target = move_target
+
     def reset_move_cycle(self):
         self._move_cycle = self.move_speed()
 
     def move(self):
-        self._move_cycle -= 1
+        """move this object to the specified location set by move_target.
+        A call to this function may do the following:
+         - not move the object at all because the unit can't move this tick yet
+         - not move the object at all because no move_target is specified
+         - move the object by 1 field in the x or y coordinate
+        This method should be called once, each tick, 
+        o the movement speed of this object can be handled properly"""
         if self._move_cycle <= 0:
             move = self.pathfind_next_move()
             if move[0] != 0 or move[1] != 0:
                 self._position = numpy.add(self._position, self._move)
                 # only reset move_cycle after we have actually moved
                 self.reset_move_cycle()
+            else:
+                # reset move target
+                move_target = None
+        else:
+            self._move_cycle -= 1
 
     def pathfind_next_move(self):
-        # simple path finding algorithm always taking the direct path
-        # this only works as long as there are no obstacles on the map
+        """simple path finding algorithm, which always takes the direct path
+        to the target. This only works as long as there are no obstacles
+        on the map."""
         if self._move_target == None:
             return (0, 0)
 
+        # check on which coordinate we need to walk longer
+        # select a random one in case both coordinates distances are equal
         distance = numpy.subtract(self._move_target, self._position)
-        if abs(distance[0]) > abs(distance[1]):
-            # move along the x axis
-            return (distance[0] / distance[0], 0)
-        elif abs(distance[0]) < abs(distance[1]):
-            # move along the y axis
-            return (0, distance[1] / distance[1])
-        else:
-            # select  a random one
-            n = random.randrange(1) == 0  
-            chooser = (n, 1 - n)
-            distance = numpy.divide(distance, distance)
-            return numpy.multiply(distance, chooser)
+        absdistance = numpy.absolute(distance)
+        is_x_longer = random.randrange(1)
+        if   absdistance[0] > absdistance[1]: is_x_longer = 1
+        elif absdistance[0] < absdistance[1]: is_x_longer = 0
+
+        # (1, 0) if x coordinate is chosen, (0, 1) if y was chosen
+        chooser = (is_x_longer, 1 - is_x_longer)
+        # you can only move one field at maximum
+        distance = numpy.sign(distance)
+        # eliminate a coordinate
+        distance = numpy.multiply(distance, chooser)
+        return tuple(distance)
 
 
---
+++ srv/tests/test_moveable.py
@@ -0,0 +1,52 @@
+import unittest
+from model.moveable import Moveable
+
+class MoveableTest(unittest.TestCase):
+    def setUp(self):
+        self.moveable = Moveable(move_speed = 2, position = (5, 5))
+
+    def test_move_speed(self):
+        self.assertEquals(2, self.moveable.move_speed)
+
+    def test_move_target(self):
+        self.assertEquals(None, self.moveable.move_target)
+        self.moveable.move_target = (1,1)
+        self.assertEquals((1,1), self.moveable.move_target)
+
+    def test_pathfind_next_move(self):
+        # no move target
+        self.assertEquals((0, 0), self.moveable.pathfind_next_move())
+
+        # nearby fields
+        self.moveable.move_target = (4,5)
+        self.assertEquals((-1, 0), self.moveable.pathfind_next_move())
+        self.moveable.move_target = (6,5)
+        self.assertEquals((1, 0), self.moveable.pathfind_next_move())
+        self.moveable.move_target = (5,6)
+        self.assertEquals((0, 1), self.moveable.pathfind_next_move())
+        self.moveable.move_target = (5,4)
+        self.assertEquals((0, -1), self.moveable.pathfind_next_move())
+
+        # multiple fields on one coordinate
+        self.moveable.move_target = (1,5)
+        self.assertEquals((-1, 0), self.moveable.pathfind_next_move())
+        self.moveable.move_target = (5,8)
+        self.assertEquals((0, 1), self.moveable.pathfind_next_move())
+
+        # multiple fields, two coordinates
+        self.moveable.move_target = (1,4)
+        self.assertEquals((-1, 0), self.moveable.pathfind_next_move())
+        self.moveable.move_target = (4,8)
+        self.assertEquals((0, 1), self.moveable.pathfind_next_move())
+
+        # same distance on both coordinates
+        self.moveable.move_target = (4,6)
+        # repeat this, because random value out of two possibilities is chosen
+        for x in range(100):
+            self.moveable.move_target = (4,6)
+            move = self.moveable.pathfind_next_move()
+            self.assertTrue(move == (0, 1) or move == (-1, 0))
+
+            self.moveable.move_target = (8,2)
+            move = self.moveable.pathfind_next_move()
+            self.assertTrue(move == (1, 0) or move == (0, -1))
