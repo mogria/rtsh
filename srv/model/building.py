5ad885339278e290ca6b3b59b47a33f549f93061
Mogria <m0gr14@gmail.com>
2016-01-27 07:48:24 +0100

    add trainer.py for building which can train units

--- srv/model/building.py
+++ srv/model/building.py
@@ -4,12 +4,14 @@ from model.nameable import Nameable
 from model.destroyable import Destroyable
 from model.positionable import Positionable
 from model.idgen import new_id
+from abc import ABCMeta, abstractmethod
 
-class Building(GameObject, Destroyable, Positionable, Nameable, Ownable):
-    def __init__(self, building_id=-1, building_type="none", *args, **kwargs):
+class Building(GameObject, Destroyable, Positionable, Nameable, Ownable, metaclass=ABCMeta):
+    def __init__(self, building_id=-1, building_type="none", usable = False, *args, **kwargs):
         super(Building, self).__init__(*args, **kwargs)
         self._building_id = building_id if building_id != -1 else new_id()
         self._building_type = building_type
+        self._usable = usable
 
     @property
     def building_id(self):
@@ -18,6 +20,16 @@ class Building(GameObject, Destroyable, Positionable, Nameable, Ownable):
     @property
     def building_type(self):
         return self._building_type
+
+    @property
+    def usable(self):
+        """wheter the building is usable. This is set to true if the building has been finished"""
+        return self._usable
+
+    @abstractmethod
+    def max_health(self):
+        """returns max health the building can have. This is also the health value at which is building is finished"""
+        pass
 
     def storage_location(self):
         return "{0}/buildings/building-{1}.json".format(Ownable.storage_location(self), self.building_id)
---
+++ srv/model/trainer.py
@@ -0,0 +1,67 @@
+from abc import ABCMeta, abstractmethod
+from model.unitfactory import UnitFactory
+from model.util import filterobject
+
+class TrainingError(Exception):
+    def __init__(self, message):
+        super(TrainingError, self).__init__(message)
+
+class Trainer(metaclass=ABCMeta):
+
+    def __init__(self, training_progress = 0, training_queue=[], *args, **kwargs):
+        super(Trainer, self).__init__(*args, **kwargs)
+        self._training_progress = training_progress
+        self._training_queue = training_queue
+
+    @property
+    def training_progress(self):
+        return self._training_progress
+
+    @property
+    def training_queue(self):
+        return self._training_queue
+
+    @abstractmethod
+    def trainable_units(self):
+        """returns a dictionary. In the form of
+        {
+            'unit_type': {
+                'trainspeed': amount_of_ticks_until_created,
+                'resource_cost': {
+                    'gold': amount_of_gold_cost,
+                    'wood': amount_of_wood_cost,
+                    'stone': amount_of_stone_cost
+                }
+            }
+            'unit_type2': ...
+        }
+        """
+        pass
+
+    def add_to_training_queue(self, unit_type):
+        if not unit_type in trainable_units():
+            raise TrainingError("cannot train unit {0}".format(unit_type))
+
+        self._training_queue.append(unit_type)
+
+    def train(self):
+        """should be called every tick, returns a Unit everytime
+        a new unit has been trained, else None is returned."""
+        if hasattr(self, 'usable') and not self.usable:
+            return
+
+        if self._training_queue.count() == 0:
+            # nothing to do
+            return
+
+        trained_unit = self._training_queue[0]
+        trained_unit_info = trainable_units()[trained_unit]
+        self._training_progress += 1
+        if self._training_progress >= trained_unit_info['trainspeed']:
+            # unit finished
+            self._training_queue = self._training_queue[1:]
+            return UnitFactory(trained_unit, filterobject(self, ['position', 'owner']))
+
+        # nothing trained this tick
+        return None
+
