b56de17522e8708f22a77ea5e9616ccbf5c910ed
Mogria <m0gr14@gmail.com>
2016-02-10 17:49:52 +0100

    extend runtests.sh, that command line options can be passed to nosetests

--- srv/runtests.sh
+++ srv/runtests.sh
@@ -3,4 +3,4 @@ LOC="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 echo "building container ... "
 echo "If you run this script the first time, this may take a while: "
 docker build --quiet -t mogria/rtsh-srv-test -f "$LOC/Dockerfile_test" "$LOC"
-docker run --rm -i -v "$LOC:/gamesrv:rw" mogria/rtsh-srv-test
+docker run --rm -i -v "$LOC:/gamesrv:rw" mogria/rtsh-srv-test "$@"
